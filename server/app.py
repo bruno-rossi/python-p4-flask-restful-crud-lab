90d083e62de5e27e942659bfb43e25050f8f743b
Bruno Rossi <brunogabrielrossi@gmail.com>
2024-02-01 12:39:39 -0500

    Submitting

--- server/app.py
+++ server/app.py
@@ -46,7 +46,29 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
 
+        if plant:
+            for attr in request.get_json():
+                setattr(plant, attr, request.get_json()[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        plant_dict = plant.to_dict()
+
+        return make_response ( plant_dict, 200 )
+    
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+
+        return make_response({"message": "record successfully deleted"}, 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
